abdf64e79733b00ac2c628be692b763fc81aefdd
Joy Wang <joywangx@sina.com>
2019-04-22 11:45:28 +0800

    alert 测试扫码

--- src/components/Page/Verify/VerifyMenu/VerifyMenu.tsx
+++ src/components/Page/Verify/VerifyMenu/VerifyMenu.tsx
@@ -14,7 +14,8 @@ class VerifyMenu extends Component<any> {
         needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
         scanType: ['qrCode', 'barCode'], // 可以指定扫二维码还是一维码，默认二者都有
         success: function(res: any) {
-          console.log(JSON.stringify(res))
+          alert(JSON.stringify(res))
+          alert(JSON.stringify(res.resultStr))
           // that.verifyForm.code = res.resultStr;
           // that.getCouponStatus(res.resultStr);
         },
